ef1c3144b16d043a533ba6d31208993f1c609ff3
Louw-C <kyss.louw@gmail.com>
2022-04-27 09:18:26 +0900

    Added code to look at tuna weight data from BOFI. Wanted to look at weight distributions - also for different species and grades.
    Developed code for different histogram graphs.

--- Code/BOFI_Data.R
+++ Code/BOFI_Data.R
@@ -12,19 +12,6 @@ library(car)
 PNMS_BOFI<-read.csv(file.choose(),header=T,sep=",")
 names(PNMS_BOFI)
 
-require(Rmisc)
-require(ggplot2)
-#Tuna length
-BOFI_Total<-summarySE(data_long, measurevar="Abundance", groupvars=c("Month", "Type"))
-BOFI1<-ggplot(BOFI_Total, aes(x=factor(Month), y=Abundance, fill=Type))+
-  geom_col(position=position_dodge(0.9))+
-  labs(y = "Average catch per month")+
-  geom_errorbar(aes(ymin=Abundance-se, ymax=Abundance+se),position=position_dodge(0.9), width=0.4)+
-  theme(legend.title = element_blank(),panel.background = element_blank(),panel.grid.major=element_line(0.5, colour="Gray80"),
-        axis.text.x = element_text(size=11),axis.title.x=element_blank(),
-        axis.text.y= element_text(size=11))
-BOFI1
-
 #Change the format of the data from wide to long
 library(tidyr)
 
@@ -36,3 +23,45 @@ library(tidyr)
 # - factor_key: Treat the new key column as a factor (instead of character vector)
 data_long <- gather(PNMS_BOFI, Type, Abundance, Big.eye:Skipjack, factor_key=TRUE)
 data_long
+
+require(Rmisc)
+require(ggplot2)
+#Total catch per month (Cannot do daily average, as number of days/month varies)
+BOFI1<-ggplot(data_long, aes(x=factor(Month), y=Abundance, fill=Type))+
+  geom_col(position=position_dodge(0.9))+
+  labs(y = "Total monthly catch")+
+  theme(legend.title = element_blank(),panel.background = element_blank(),panel.grid.major=element_line(0.5, colour="Gray80"),
+        axis.text.x = element_text(size=11),axis.title.x=element_blank(),
+        axis.text.y= element_text(size=11))
+BOFI1
+
+#Order the x-axis labels
+data_long$Month <- factor(data_long$Month, levels = c("June","July","August", "September","October", "November","December","January","February", "March"))
+
+
+##BOFI tuna weights
+BOFI_weight<-read.csv(file.choose(),header=T,sep=",")
+names(BOFI_weight)
+hist(BOFI_weight$Weight_kg)
+
+#Remove Albacore
+
+sans_albacore<-subset(BOFI_weight, Species!="Albacore")
+sans_albacore<-subset(sans_albacore, Grade!="Not graded")
+
+ggplot(sans_albacore, aes(x = Weight_kg,)) +
+  geom_histogram(fill = "white", colour = "black")
+
+ggplot(sans_albacore, aes(x = Weight_kg,)) +
+  geom_histogram(fill = "white", colour = "black") +
+  facet_grid(Species~.)
+
+ggplot(sans_albacore, aes(x = Weight_kg,)) +
+  geom_histogram(fill = "white", colour = "black") +
+  facet_grid(Species~Grade)
+
+ggplot(BOFI_weight, aes(x = Weight_kg, fill=Species)) +
+  geom_histogram(fill = "white", colour = "black") +
+  facet_grid(Grade~.)
+
+sans_albacore$Grade <- factor(sans_albacore$Grade, levels = c("A","B+","B","R"))
